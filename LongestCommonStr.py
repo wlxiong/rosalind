03b645966b4002dc1175c9dff1463fdf31a9b34b
Yiliang Xiong <wlxiong@gmail.com>
2013-01-04 14:54:39 +0800

    Find the longest commont substring of multiple strings
    using suffix array and a min deque

--- LongestCommonStr.py
+++ LongestCommonStr.py
@@ -1,9 +1,13 @@
-infile = open("longest_common_substr.txt")
-outfile = open("longest_common_substr_out.txt", 'w')
+infile = open("rosalind_lcs.txt")
+outfile = open("rosalind_lcs_output.txt", 'w')
 
 DNAs = []
 for line in infile:
 	DNAs.append(line.strip())
+
+if len(DNAs) == 1:
+	print >> outfile, DNAs[0]
+	exit()
 
 suffix = []
 for k, t in enumerate(DNAs):
@@ -11,27 +15,71 @@ for k, t in enumerate(DNAs):
 		suffix.append((t[i:], k))
 
 sorted_suffix = sorted(suffix)
-print "\n".join(map(str, sorted_suffix))
+# print "\n".join(map(str, sorted_suffix))
+
 
 def len_common_prefix(a, b):
-	for i in range(min(len(a), len(b))):
+	min_len = min(len(a), len(b))
+	for i in range(min_len):
 		if a[i] != b[i]:
-			return i
-	return min(len(a), len(b))
+			return (i, a[:i])
+	return (min_len, a[:min_len])
 
-len_prefix = []
+common_prefix = []
 for i in range(len(sorted_suffix)-1):
-	len_prefix = len_common_prefix(sorted_suffix[i], sorted_suffix[i+1])
-	
+	common_prefix.append(len_common_prefix(sorted_suffix[i][0], sorted_suffix[i+1][0]))
 
-in_window = [0] * len(DNAs)
-no_all = 0
+
+from collections import deque
+min_deque = deque()		# define a min queue
+
+def push_deque(elem):
+	while len(min_deque) > 0 and min_deque[-1] > elem:
+		min_deque.pop()
+	min_deque.append(elem)
+
+def top_deque():
+	if len(min_deque) == 0:
+		raise Exception("deque is empty")
+	return min_deque[0]
+
+def pop_deque(elem):
+	if elem == top_deque():
+		min_deque.popleft()
+
+
+DNA_in_window = 0
+is_in_window = [0] * len(DNAs)
+
+def add_to_window(i):
+	global DNA_in_window
+	suf, k = sorted_suffix[i]
+	if is_in_window[k] == 0:
+		DNA_in_window += 1
+	is_in_window[k] += 1
+
+def del_from_window(i):
+	global DNA_in_window
+	suf, k = sorted_suffix[i]
+	is_in_window[k] -= 1
+	if is_in_window[k] == 0:
+		DNA_in_window -=1
+
 start = 0
 end = 0
+longest_prefix = ""
+add_to_window(0)
 while end < len(sorted_suffix):
-	if no_all < len(DNAs):
+	if DNA_in_window < len(DNAs):
 		end += 1
-		suf, k = sorted_suffix[end]
-		if in_window[k] == 0:
-			no_all += 1
-		in_window[k] += 1
+		if end < len(sorted_suffix):
+			add_to_window(end)
+			push_deque(common_prefix[end-1])
+	else:
+		prefix = top_deque()
+		longest_prefix = max(longest_prefix, prefix)
+		del_from_window(start)
+		pop_deque(common_prefix[start])
+		start += 1
+
+print >> outfile, longest_prefix[1]
